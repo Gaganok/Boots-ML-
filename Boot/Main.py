ee2e4cb0c53bb13df57565e188a8044e92909603
Oleh <oleh.kepsha@mycit.ie>
2019-11-12 22:34:30 +0000

    added cross validation

--- Boot/Main.py
+++ Boot/Main.py
@@ -1,12 +1,12 @@
 import numpy as np
-import scipy.misc as smp
 import pandas as ps;
-from PIL import Image
+
+from sklearn.model_selection import cross_validate
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import Perceptron
-from sklearn.linear_model import LogisticRegression
 from sklearn.datasets import load_digits
+import matplotlib.pyplot as plt
 
 def main():
     #Load
@@ -30,10 +30,16 @@ def main():
     print("Boots Samples: " + str(len(boots)))
     
     #Display Images
-    #display(sneaker, "sneaker");
-    #display(boot, "boot");
+    display(sneaker, "sneaker");
+    display(boot, "boot");
     
-    trainX, testX, trainY, testY = train_test_split(data, sneakers_targer, test_size=0.3);
+    #sneaker = sneakers.values[0];
+    #sneaker = np.delete(sneaker, 0)
+    #np.reshape(sneaker, (28, 28))
+    #print(sneaker)
+    
+    k = 5;
+    trainX, testX, trainY, testY = train_test_split(data, sneakers_targer, test_size=1/5);
     #train, test = train_test_split(data, test_size = 0.3);
     
     
@@ -42,21 +48,31 @@ def main():
     print(len(testX));
     print(len(testY))
     
-    #print(cross_val_score(Perceptron(), train, test));
-    pr = Perceptron();
-    pr.fit(trainX, trainY);
-    print(pr.score(testX, testY));
+    #scoring = ['precision_macro', 'recall_macro'];
+    score = cross_validate(Perceptron(), data, sneakers_targer, cv = k)
+    print(score);
+    #print (score.mean());
+    #print(cross_val_score(SVC(), data, sneakers_targer, cv = k));
+    
+    #pr = Perceptron();
+    #pr.fit(trainX, trainY);
+    #print(pr.score(testX, testY));
     
     #lr = LogisticRegression()
     #lr.fit(trainX, trainY);
     #print(lr.score(testX, testY));
     
-    
+def eval(model, x, y, k):
+    score = cross_val_score(model, x, y, cv = k);
+
 def display(pixelArray, title):
     del pixelArray[:1]
-    im = Image.new('L', (28, 28))
-    im.putdata(tuple(pixelArray));
-    im.show()
+    #im = Image.new('L', (28, 28))
+    #im.putdata(tuple(pixelArray));
+    #arr = np.asarray()
+    plt.imshow(np.reshape(pixelArray, (28, 28)).reshape(28, 28), cmap='gray', vmin=0, vmax=255)
+    plt.show()
+    #im.show()
    
    
 '''
