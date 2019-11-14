2791b0173c5a34318e586436cb3960cf5462ee8f
Oleh <oleh.kepsha@mycit.ie>
2019-11-14 00:06:07 +0000

    Replaced cross_valid function

--- Boot/Main.py
+++ Boot/Main.py
@@ -4,17 +4,15 @@ import pandas as ps;
 from sklearn.model_selection import cross_validate
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
 from sklearn.linear_model import Perceptron
 from sklearn.datasets import load_digits
 import matplotlib.pyplot as plt
+import time
 
 def main():
     #Load
     data = ps.read_csv("product_images.csv");
-    
-    digits = load_digits(True);
-    #print(digits.data)
-    print(digits.target)
     
     #Separate Labels
     boots = data.loc[data['label'] == 1];
@@ -30,27 +28,30 @@ def main():
     print("Boots Samples: " + str(len(boots)))
     
     #Display Images
-    display(sneaker, "sneaker");
-    display(boot, "boot");
-    
-    #sneaker = sneakers.values[0];
-    #sneaker = np.delete(sneaker, 0)
-    #np.reshape(sneaker, (28, 28))
-    #print(sneaker)
+    #display(sneaker, "sneaker");
+    #display(boot, "boot");
+
     
     k = 5;
-    trainX, testX, trainY, testY = train_test_split(data, sneakers_targer, test_size=1/5);
+    
+    #trainX, testX, trainY, testY = train_test_split(x, y, test_size=1/k);
+    
+    fit_time, score_time, score = eval(Perceptron(), data, sneakers_targer, k);
+    print(eval(Perceptron(), data, sneakers_targer, k));
+    print(score_time);
+    print(score);
+    #trainX, testX, trainY, testY = train_test_split(data, sneakers_targer, test_size=1/5);
     #train, test = train_test_split(data, test_size = 0.3);
     
     
-    print(len(trainX));
-    print(len(trainY));
-    print(len(testX));
-    print(len(testY))
+    #print(len(trainX));
+    #print(len(trainY));
+    #print(len(testX));
+    #print(len(testY))
     
     #scoring = ['precision_macro', 'recall_macro'];
-    score = cross_validate(Perceptron(), data, sneakers_targer, cv = k)
-    print(score);
+    #score = cross_validate(Perceptron(), data, sneakers_targer, cv = k)
+    #print(score);
     #print (score.mean());
     #print(cross_val_score(SVC(), data, sneakers_targer, cv = k));
     
@@ -63,7 +64,30 @@ def main():
     #print(lr.score(testX, testY));
     
 def eval(model, x, y, k):
-    score = cross_val_score(model, x, y, cv = k);
+    
+    fit_time = [];
+    score_time = [];
+    score = [];
+    
+    k_fold = KFold(n_splits = k);
+    for x_index, y_index in k_fold.split(x):
+        trainX, testX, trainY, testY = x.values[x_index], x.values[y_index], y[x_index], y[y_index];
+        
+        start = time.perf_counter();
+        model.fit(trainX, trainY);
+        end = time.perf_counter();
+        
+        fit_time.append(end - start);
+        
+        start = time.perf_counter();
+        s = model.score(testX, testY)
+        end = time.perf_counter();
+        
+        score_time.append(end - start);
+        
+        score.append(s);
+    
+    return fit_time, score_time, score;
 
 def display(pixelArray, title):
     del pixelArray[:1]
